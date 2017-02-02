4effa5483613023466be8b7103478baa0497aaed
Amir Hadzic <amir.hadzic@randomshouting.com>
2017-02-02 15:01:19 +0100

    Remove table-responsive wrapper

--- src/Table.js
+++ src/Table.js
@@ -30,31 +30,29 @@ class Table extends Component {
     const visibleColumns = columns.filter((c) => !c.hidden);
 
     return (
-      <div className="table-responsive">
-        <table className={`table ${className}`}>
-          <thead>
-            <tr>
-              {selectable &&
-                <SelectAllHeader
-                  {...headers.select}
-                  CheckboxComponent={CheckboxComponent}
-                />
+      <table className={`table ${className}`}>
+        <thead>
+          <tr>
+            {selectable &&
+              <SelectAllHeader
+                {...headers.select}
+                CheckboxComponent={CheckboxComponent}
+              />
+            }
+            {visibleColumns.map((col) => {
+              if (col.sortable && !col.hidden) {
+                return (<SortableHeader key={col.key} {...headers[col.key]} />);
               }
-              {visibleColumns.map((col) => {
-                if (col.sortable && !col.hidden) {
-                  return (<SortableHeader key={col.key} {...headers[col.key]} />);
-                }
-                return (<th data-key={col.key} key={col.key}>{col.header}</th>);
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row) => (
-              <TableRow key={_.get(row, primaryKey)} {...this.props} row={row} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+              return (<th data-key={col.key} key={col.key}>{col.header}</th>);
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <TableRow key={_.get(row, primaryKey)} {...this.props} row={row} />
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
