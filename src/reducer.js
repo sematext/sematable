3f29cc4bd79c30732d0ff14203d0bfeb630cc961
Davor Obilinović <Obee88@gmail.com>
2018-08-22 13:47:17 +0200

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -151,7 +151,7 @@ const behaviours = {
   },
 };
 
-const tableReducer = handleActions(behaviours);
+const tableReducer = handleActions(behaviours, {});
 
 export default (state, action) => {
   if (!state) {
